d86d1d76db311a248b521c6665ad190e4921f239
Ashish Yadav <info@ashish-yadav.com>
2026-03-22 18:59:10 -0400

    Update ingest.py to handle both PDF and TXT files

--- ingest.py
+++ ingest.py
@@ -1,6 +1,6 @@
 import os
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 import chromadb
 from chromadb.utils import embedding_functions
 
